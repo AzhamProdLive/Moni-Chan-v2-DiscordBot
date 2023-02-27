afd6c6bbf4bb83ebb99748f2fd1f36d24b1c4e9f
Azham Makes Trash <73955243+AzhamProdLive@users.noreply.github.com>
2023-02-27 15:30:19 +0100

    Fix indents in files

--- commands/options-info.js
+++ commands/options-info.js
@@ -1,13 +1,13 @@
 const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('options-info')
-        .setDescription('Make Moni-Chan say something!')
-        .addStringOption(option => option.setName('input').setDescription('The input to echo back')),
-    async execute(interaction) {
-        const value = interaction.options.getString('input');
-        if (value) return interaction.reply(`${value}`);
-        return interaction.reply('No option was provided!');
-    },
+	data: new SlashCommandBuilder()
+		.setName('options-info')
+		.setDescription('Make Moni-Chan say something!')
+		.addStringOption(option => option.setName('input').setDescription('The input to echo back')),
+	async execute(interaction) {
+		const value = interaction.options.getString('input');
+		if (value) return interaction.reply(`${value}`);
+		return interaction.reply('No option was provided!');
+	},
 };
--- interactionCreate.js
+++ interactionCreate.js
@@ -1,13 +1,14 @@
 const { Events } = require('discord.js');
 
 module.exports = {
-    name: 'interactionCreate',
-    async execute(interaction) {
-        try {
-            await command.execute(interaction);
-        } catch (error) {
-            console.error(`Error executing ${interaction.commandName}`);
-            console.error(error);
-        }
-    },
+	name: 'interactionCreate',
+	async execute(interaction) {
+		try {
+			await command.execute(interaction);
+		}
+		catch (error) {
+			console.error(`Error executing ${interaction.commandName}`);
+			console.error(error);
+		}
+	},
 };
