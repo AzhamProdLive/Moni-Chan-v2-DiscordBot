e03c78c6cd5816370e35028aa3b28f6f0585137e
Azham Makes Trash <73955243+AzhamProdLive@users.noreply.github.com>
2023-02-28 14:55:16 +0100

    Created and editing kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -1,12 +1,35 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder, PermissionFlagsBits } = require('discord.js');
+
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('kick')
-		.setDescription('Select a member and kick them (but not really).')
-		.addUserOption(option => option.setName('target').setDescription('The member to kick')),
-	async execute(interaction) {
-		const member = interaction.options.getMember('target');
-		return interaction.reply({ content: `You wanted to kick: ${member.user.username}`, ephemeral: true });
+		.setDescription('Kick a user from the server.')
+		.setDefaultMemberPermissions(PermissionFlagsBits.KickMembers)
+		.addUserOption(option =>
+			option.setName('target')
+				.setDescription('The member to kick')
+				.setRequired(true))
+		.addStringOption(option =>
+			option.setName('reason')
+				.setDescription('Reason for kicking the member')),
+	execute: async function(interaction) {
+		const { channel, options } = interaction;
+		const user = options.getUser('target');
+		const reason = options.getString('reason');
+		const member = await interaction.guild.members.fetch(user.id);
+		const errEmbed = new EmbedBuilder()
+			.setDescription('You do not have permission to kick this user, since they have a higher role!')
+			.setColor(0xff0000);
+		if (member.roles.highest.position >= interaction.member.roles.highest.position) {
+			return interaction.reply({ embeds: [errEmbed], ephemeral: true });
+		}
+		await member.kick(reason);
+
+		const embed = new EmbedBuilder()
+			.setDescription(`Successfully kicked ${user} from the server for the following reason : ${reason}!`)
+			.setColor(0x00ff00);
+		await interaction.reply({ embeds: [embed] });
 	},
 };
+
