e3ba484f8f778a986ea5de7824f3f89651d534d9
Azham Makes Trash <73955243+AzhamProdLive@users.noreply.github.com>
2023-02-28 20:07:25 +0100

    Added a comment and removed deploy-commands.js from events to main tree

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,4 +1,4 @@
-const fs = require('node:fs');
+/* const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('../config.json');
@@ -15,21 +15,24 @@ for (const file of commandFiles) {
 	client.commands.set(command.data.name, command);
 }
 console.log(`Successfully reloaded ${commandFiles.length} application (/) commands.`);
-client.login(token);
+client.login(token);*/
 
+// Commentary up there was a test that isn't going to be used, or maybe if really needed ig
 
-/* const { REST, Routes } = require('discord.js');
-const { clientId, guildId, token } = require('../config.json');
+const { REST, Routes } = require('discord.js');
+const { clientId, guildId, token } = require('./config.json');
 const fs = require('node:fs');
+const path = require('node:path');
 
 const commands = [];
 // Grab all the command files from the commands directory you created earlier
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
 // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
 for (const file of commandFiles) {
-    const command = require(`../commands/${file}`);
-    commands.push(command.data.toJSON());
+	const command = require(`./commands/${file}`);
+	commands.push(command.data.toJSON());
 }
 
 // Construct and prepare an instance of the REST module
@@ -37,18 +40,19 @@ const rest = new REST({ version: '10' }).setToken(token);
 
 // and deploy your commands!
 (async () => {
-    try {
-        console.log(`Started refreshing ${commands.length} application (/) commands.`);
+	try {
+		console.log(`Started refreshing ${commands.length} application (/) commands.`);
 
-        // The put method is used to fully refresh all commands in the guild with the current set
-        const data = await rest.put(
-            Routes.applicationGuildCommands(clientId, guildId),
-            { body: commands },
-        );
+		// The put method is used to fully refresh all commands in the guild with the current set
+		const data = await rest.put(
+			Routes.applicationGuildCommands(clientId, guildId),
+			{ body: commands },
+		);
 
-        console.log(`Successfully reloaded ${data.length} application (/) commands.`);
-    } catch (error) {
-        // And of course, make sure you catch and log any errors!
-        console.error(error);
-    }
-})();*/
+		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+	}
+	catch (error) {
+		// And of course, make sure you catch and log any errors!
+		console.error(error);
+	}
+})();
