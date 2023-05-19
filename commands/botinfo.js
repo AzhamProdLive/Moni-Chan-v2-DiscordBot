666639cda032013f08ee07f98295dcdf93248970
azhmprd <legamer0007@gmail.com>
2023-05-19 19:26:44 +0200

    new command added, botinfo.js, fixed packages issues, and now we work

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -1,26 +1,33 @@
 const { SlashCommandBuilder } = require('discord.js');
 const { EmbedBuilder } = require('discord.js');
+const cpuStat = require('cpu-stat');
 const os = require('os');
-const ms = require('ms');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('stats')
 		.setDescription('Shows the stats of the bot'),
-	async execute(interaction) {
-		const embed = new EmbedBuilder()
-			.setTitle('Bot Stats')
-			.addFields(
-				{ name: 'Server Count', value: `${interaction.client.guilds.cache.size}`, inline: true },
-				{ name: 'Uptime', value: `${ms(interaction.client.uptime)}`, inline: true },
-				{ name: 'RAM Usage', value: `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`, inline: true },
-				{ name: 'CPU Usage', value: `${(process.cpuUsage().system / 1024 / 1024).toFixed(2)}%`, inline: true },
-				{ name: 'Operating System', value: `${os.platform()}`, inline: true },
-				{ name: 'Node.js Version', value: `${process.version}`, inline: true },
-				{ name: 'Discord.js Version', value: `v${require('discord.js').version}`, inline: true },
-			)
-			.setFooter(`Requested by ${interaction.user.tag}`, interaction.user.displayAvatarURL({ dynamic: true }))
-			.setTimestamp();
-		await interaction.reply({ embeds: [embed] });
+	execute(interaction, client) {
+		cpuStat.usagePercent(function(err, percent) {
+			if (err) return interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+
+			const embed = new EmbedBuilder()
+				.setTitle('Bot Stats')
+				.setColor('#31d668')
+				.addFields(
+					{ name: 'Server Count', value: `${interaction.client.guilds.cache.size}`, inline: true },
+					{ name: 'Uptime', value: `${Math.floor(process.uptime() / 3600)} hours, ${Math.floor(process.uptime() / 60)} minutes, ${Math.floor(process.uptime() % 60)} seconds`, inline: true },
+					{ name: 'RAM Usage', value: `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`, inline: true },
+					{ name: 'CPU Usage', value: `${(process.cpuUsage().system / 1024 / 1024).toFixed(2)}%`, inline: true },
+					{ name: 'Operating System', value: `Arch Linux`, inline: true },
+					{ name: 'Node.js Version', value: `${process.version}`, inline: true },
+					{ name: 'Discord.js Version', value: `v${require('discord.js').version}`, inline: true },
+					{ name: 'Developer', value: 'Maximus Decimus#3263', inline: true },
+					{ name: 'Bot Ping', value: (interaction.client.ws.ping) + 'ms', inline: true },
+				)
+				.setFooter({ text: 'Made by Maximus Decimus#3263' })
+				.setTimestamp();
+			interaction.reply({ embeds: [embed] });
+		});
 	},
 };
