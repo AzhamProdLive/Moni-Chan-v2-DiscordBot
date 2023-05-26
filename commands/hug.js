0ed745a7d41422bcca8082ef9fa121dd42161ac1
azhmprd <legamer0007@gmail.com>
2023-05-26 15:45:07 +0200

    Hug command upgraded !

--- commands/hug.js
+++ commands/hug.js
@@ -1,6 +1,10 @@
 // Description: Sends a hug gif to the user.
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { EmbedBuilder } = require('discord.js');
+const randomhugs = [
+	'https://media.discordapp.net/attachments/930207359232512060/999396292243509368/MonikaHug2.gif',
+	'https://media.tenor.com/yz_nVDTQ-TgAAAAd/monika-ddlc.gif',
+	'https://media.tenor.com/s_voIurxH_MAAAAC/ddlc-monika.gif'];
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -11,8 +15,8 @@ module.exports = {
 		const user = interaction.options.getUser('user');
 		const embed = new EmbedBuilder()
 			.setTitle(`${interaction.user.username} hugged ${user.username}!`)
-			.setImage('https://media.discordapp.net/attachments/930207359232512060/999396292243509368/MonikaHug2.gif')
-			.setColor('#FF0000');
+			.setImage(randomhugs[Math.floor(Math.random() * randomhugs.length)])
+			.setColor('#31d668');
 		await interaction.reply({ embeds: [embed] });
 	},
 };
