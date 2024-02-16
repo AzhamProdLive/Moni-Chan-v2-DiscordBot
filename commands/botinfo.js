2890759ff6e6da0421f0ba9f47eaa3b172faebc0
Azham Makes Trash <azhamproductions@proton.me>
2024-02-16 21:06:40 +0100

    Up: Changes in README.md (please read)

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -2,8 +2,8 @@ const { SlashCommandBuilder } = require('discord.js');
 const { EmbedBuilder } = require('discord.js');
 const { hyperlink } = require('discord.js');
 const cpuStat = require('cpu-stat');
-const url = 'https://discord.gg/XKs9YUXdbE';
-const link = hyperlink('Support Server !', url);
+const url = '';
+const link = hyperlink('Support Server', url);
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('stats')
@@ -17,10 +17,9 @@ module.exports = {
 				.setColor('#31d668')
 				.addFields(
 					{ name: 'Server Count', value: `${interaction.client.guilds.cache.size}`, inline: true },
-					{ name: 'Uptime', value: `${Math.floor(process.uptime() / 3600)} hours, ${Math.floor((process.uptime() / 60)) } minutes, ${Math.floor(process.uptime() % 60)} seconds`, inline: true },
 					{ name: 'RAM Usage', value: `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`, inline: true },
 					{ name: 'CPU Usage', value: `${(process.cpuUsage().system / 1024 / 1024).toFixed(2)}%`, inline: true },
-					{ name: 'Operating System', value: 'Arch Linux', inline: true },
+					{ name: 'Operating System', value: os.type(), inline: true },
 					{ name: 'Node.js Version', value: `${process.version}`, inline: true },
 					{ name: 'Discord.js Version', value: `v${require('discord.js').version}`, inline: true },
 					{ name: 'Developer', value: 'Maximus Decimus#3263 & DJMayJay#2001', inline: true },
